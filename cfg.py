85987007c4bb6a4c4892827b2f3d8bfec5271519
DEV <maitrungkien1296@gmail.com>
2024-06-28 23:29:26 +0700

    init

---
+++ cfg.py
@@ -0,0 +1,21 @@
+import torch
+import tqdm
+import glob
+import os
+import shutil
+
+from ultralytics import YOLOv10
+
+config = {
+    "yaml_path": r"Safety_Helmet_Dataset\data.yaml",
+    "model_path" : r"weights\yolov10s.pt" , 
+    "epochs": 100,
+    "img_size": 640,
+    "batch_size":128
+}
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu"); print(f"device: {device}")
+
+MODEL_PATH = "weights/yolov10l.pt"
+MODEL_HELMET_PATH = "custom_weights/best.pt"
+RESULT_PATH = "result/"
---
+++ train.py
@@ -0,0 +1,14 @@
+from cfg import YOLOv10,  device, config
+def train():
+
+    print(f'device: {device}')
+    model = YOLOv10 (config["model_path"]);model.to(device)
+    
+    model.train( data   = config["yaml_path"] ,
+                epochs  = config["epochs"],
+                batch   = config["batch_size"],
+                imgsz   = config["img_size"] )
+
+
+if __name__ == "__main__":
+    train()
